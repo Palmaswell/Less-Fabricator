ca9a3af79214aaa4a997ae375b6652bc9b3cd6dd
Mauricio Palma <me@palmaswell.com>
2014-12-17 13:23:27 +0100

    Initital setup and Less integration

---
+++ README.md
@@ -0,0 +1,65 @@
+
+# Less Fabricator
+
+> _fabricate_ - to make by assembling parts or sections.
+
+Less Fabricator is a tool for creating modular website toolkits in Less and not Sass - _think ["Tiny Bootstraps, for Every Client"](http://daverupert.com/2013/04/responsive-deliverables/#tiny-bootstraps-for-every-client)_
+
+Based on [fabricator](http...) by ...
+
+## Usage
+
+Getting started
+---------------------
+
+For the sake of your Node project management we recommend you use NVM.
+[Node Version Manager](https://github.com/creationix/nvm)
+
+### Install gulp
+
+```
+npm install --global gulp
+```
+
+### Install all dependencies
+
+```
+npm install
+```
+gulp commands
+---------------------
+
+### Build entire project
+
+```
+npm install && gulp
+```
+
+
+### Styleguide development (watch)
+
+Launch local development server, watching for css and styleguide changes
+
+```
+gulp --dev
+```
+
+## Documentation
+
+[Check out the wiki](https://github.com/resource/fabricator/wiki).
+
+## Credits
+
+Created by [Luke Askew](http://lukeaskew.com).
+
+Less Integration by [Mauricio Palma](http://palmaswell.com)
+
+Logo by Tim Vonderloh.
+
+Icons borrowed from [SteadySets](http://dribbble.com/shots/929153-Steady-set-of-icons?list=show) and [Font Awesome](http://fortawesome.github.io/Font-Awesome/).
+
+Code syntax highlighting by [Prism](http://prismjs.com/).
+
+## License
+
+[Copyright (c) 2014 Resource LLC](https://github.com/resource/fabricator/blob/master/LICENSE.md)
---
+++ gulpfile.js
@@ -0,0 +1,213 @@
+'use strict';
+
+// modules
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+var collate = require('./tasks/collate');
+var compile = require('./tasks/compile');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var prefix = require('gulp-autoprefixer');
+var Q = require('q');
+var rename = require('gulp-rename');
+var reload = browserSync.reload;
+var runSequence = require('run-sequence');
+var less = require('gulp-less');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+
+
+// configuration
+var config = {
+	dev: gutil.env.dev,
+	src: {
+		scripts: {
+			fabricator: [
+				'./src/fabricator/scripts/prism.js',
+				'./src/fabricator/scripts/fabricator.js'
+			],
+			toolkit: './src/toolkit/assets/scripts/toolkit.js'
+		},
+		styles: {
+			fabricator: './src/fabricator/styles/fabricator.less',
+			toolkit: './src/toolkit/assets/styles/toolkit.less'
+		},
+		images: 'src/toolkit/assets/images/**/*',
+		views: './src/toolkit/views/*.html',
+		materials: [
+			'components',
+			'structures',
+			'templates',
+			'documentation'
+		]
+	},
+	dest: './dist'
+};
+
+
+// clean
+gulp.task('clean', function (cb) {
+	del([config.dest], cb);
+});
+
+
+// styles
+gulp.task('styles:fabricator', function () {
+	return gulp.src(config.src.styles.fabricator)
+		.pipe(plumber())
+		.pipe(less({
+			errLogToConsole: true
+		}))
+		.pipe(prefix('last 2 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(rename('f.css'))
+		.pipe(gulp.dest(config.dest + '/fabricator/styles'))
+		.pipe(gulpif(config.dev, reload({stream:true})));
+});
+
+gulp.task('styles:toolkit', function () {
+	return gulp.src(config.src.styles.toolkit)
+		.pipe(plumber())
+		.pipe(less({
+			errLogToConsole: true
+		}))
+		.pipe(prefix('last 2 version'))
+		.pipe(gulpif(!config.dev, csso()))
+		.pipe(gulp.dest(config.dest + '/toolkit/styles'))
+		.pipe(gulpif(config.dev, reload({stream:true})));
+});
+
+gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
+
+
+// scripts
+gulp.task('scripts:fabricator', function () {
+	return gulp.src(config.src.scripts.fabricator)
+		.pipe(plumber())
+		.pipe(concat('f.js'))
+		.pipe(gulpif(!config.dev, uglify()))
+		.pipe(gulp.dest(config.dest + '/fabricator/scripts'));
+});
+
+gulp.task('scripts:toolkit', function () {
+	return browserify(config.src.scripts.toolkit).bundle()
+		.pipe(plumber())
+		.pipe(source('toolkit.js'))
+		.pipe(gulpif(!config.dev, streamify(uglify())))
+		.pipe(gulp.dest(config.dest + '/toolkit/scripts'));
+});
+
+gulp.task('scripts', ['scripts:fabricator', 'scripts:toolkit']);
+
+
+// images
+gulp.task('images', ['favicon'], function () {
+	return gulp.src(config.src.images)
+		.pipe(imagemin())
+		.pipe(gulp.dest(config.dest + '/toolkit/images'));
+});
+
+gulp.task('favicon', function () {
+	return gulp.src('./src/favicon.ico')
+		.pipe(gulp.dest(config.dest));
+});
+
+
+// collate
+gulp.task('collate', function () {
+
+	// 'collate' is a little different -
+	// it returns a promise instead of a stream
+
+	var deferred = Q.defer();
+
+	var opts = {
+		materials: config.src.materials,
+		dest: config.dest + '/fabricator/data/data.json'
+	};
+
+	// run the collate task; resolve deferred when complete
+	collate(opts, deferred.resolve);
+
+	return deferred.promise;
+
+});
+
+// assembly
+gulp.task('assemble:fabricator', function () {
+	var opts = {
+		data: config.dest + '/fabricator/data/data.json',
+		template: false
+	};
+
+	return gulp.src(config.src.views)
+		.pipe(compile(opts))
+		.pipe(gulp.dest(config.dest));
+});
+
+gulp.task('assemble:templates', function () {
+	var opts = {
+		data: config.dest + '/fabricator/data/data.json',
+		template: true
+	};
+	return gulp.src('./src/toolkit/templates/*.html')
+		.pipe(compile(opts))
+		.pipe(rename({
+			prefix: 'template-'
+		}))
+		.pipe(gulp.dest(config.dest));
+});
+
+gulp.task('assemble', ['collate'], function () {
+	gulp.start('assemble:fabricator', 'assemble:templates');
+});
+
+
+// server
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: config.dest
+		},
+		notify: false
+	});
+});
+
+
+// watch
+gulp.task('watch', ['browser-sync'], function () {
+	gulp.watch('src/toolkit/{components,structures,templates,documentation,views}/**/*.{html,md}', ['assemble', browserSync.reload]);
+	gulp.watch('src/fabricator/styles/**/*.less', ['styles:fabricator']);
+	gulp.watch('src/toolkit/assets/styles/**/*.less', ['styles:toolkit']);
+	gulp.watch('src/fabricator/scripts/**/*.js', ['scripts:fabricator', browserSync.reload]);
+	gulp.watch('src/toolkit/assets/scripts/**/*.js', ['scripts:toolkit', browserSync.reload]);
+	gulp.watch(config.src.images, ['images', browserSync.reload]);
+});
+
+
+// default build task
+gulp.task('default', ['clean'], function () {
+
+	// define build tasks
+	var tasks = [
+		'styles',
+		'scripts',
+		'images',
+		'assemble'
+	];
+
+	// run build
+	runSequence(tasks, function () {
+		if (config.dev) {
+			gulp.start('watch');
+		}
+	});
+
+});
